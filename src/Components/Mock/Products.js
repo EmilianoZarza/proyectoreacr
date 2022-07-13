fa5cbbe38d3c954976c4951c4b7fd87bbc6c4f04
Zarza Emiliano <zarza.emi94@gmail.com>
2022-07-12 21:59:16 -0300

    aplicando filter

--- src/Components/Mock/Products.js
+++ src/Components/Mock/Products.js
@@ -1,58 +1,82 @@
 export const Products = [
-    {
+    { 
+      type: "Producto", 
       name: "Cerveza Hipatia IPA",
       price: 250,
       stock: 10,
       id: 1,
       url: "https://images.pexels.com/photos/5530250/pexels-photo-5530250.jpeg?auto=compress&cs=tinysrgb&w=600"
     },
-    {
+    { 
+      type: "Producto", 
       name: "Cerveza Hipatia HONEY",
       price: 250,
       stock: 5,
       id: 2,
       url: "https://images.pexels.com/photos/1672304/pexels-photo-1672304.jpeg?auto=compress&cs=tinysrgb&w=600"
     },
-    {
+    { 
+      type: "Producto", 
       name: "Cerveza Hipatia BLACK",
       price: 250,
       stock: 8,
       id: 3,
       url: "https://images.pexels.com/photos/5659753/pexels-photo-5659753.jpeg?auto=compress&cs=tinysrgb&w=600"
     },
-    {
+    { 
+      type: "Producto",
       name: "Cerveza Hipatia VAINILLA",
       price: 250,
       stock: 7,
       id: 4,
       url: "https://images.pexels.com/photos/52994/beer-ale-bitter-fermented-52994.jpeg?auto=compress&cs=tinysrgb&w=600"
     },
-    {
+    { 
+      type: "Producto", 
       name: "Cerveza Hipatia RED",
       price: 250,
       stock: 8,
-      id: 6,
+      id: 5,
       url: "https://images.pexels.com/photos/4968338/pexels-photo-4968338.jpeg?auto=compress&cs=tinysrgb&w=600"
     },
-    {
+    { 
+      type: "Producto", 
       name: "Cerveza Hipatia ORANGE",
       price: 250,
       stock: 8,
       id: 6,
       url: "https://images.pexels.com/photos/6554953/pexels-photo-6554953.jpeg?auto=compress&cs=tinysrgb&w=600"
     },
-    {
+    { 
+      type: "Producto", 
       name: "Cerveza Hipatia CLASICA",
       price: 250,
       stock: 8,
-      id: 6,
+      id: 7,
       url: "https://images.pexels.com/photos/5530262/pexels-photo-5530262.jpeg?auto=compress&cs=tinysrgb&w=600"
     },
-    {
+    { 
+      type: "Producto",   
       name: "Cerveza Hipatia CACAO",
       price: 250,
       stock: 8,
-      id: 6,
+      id: 8,
       url: "https://images.pexels.com/photos/5659493/pexels-photo-5659493.jpeg?auto=compress&cs=tinysrgb&w=600"
-    }, 
+    },
+    { 
+      type: "Servicio",   
+      name: "Cerveza Hipatia BARRIL",
+      price: 3250,
+      stock: 18,
+      id: 9,
+      url: "https://images.pexels.com/photos/1267700/pexels-photo-1267700.jpeg?auto=compress&cs=tinysrgb&w=600"
+    },
+    { 
+      type: "Servicio",   
+      name: "Etiquetamos con tus diseños personalizados",
+      price: 750,
+      stock: 50,
+      id: 9,
+      url: "https://images.pexels.com/photos/5532831/pexels-photo-5532831.jpeg?auto=compress&cs=tinysrgb&w=600"
+    },
   ];
