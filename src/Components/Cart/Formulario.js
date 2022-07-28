e5910bbf57812ea8980acfcd57ebdf43fde15ada
Zarza Emiliano <zarza.emi94@gmail.com>
2022-07-27 23:29:01 -0300

    firebase II

--- src/Components/Cart/Formulario.js
+++ src/Components/Cart/Formulario.js
@@ -22,6 +22,11 @@ import { cartContext } from "../Context/CartContext";
             })
         }
 
+        let total = 0;
+        for(let i = 0; i < products.length; i++){
+        total += products[i].price * products[i].qty;
+    }
+
         const finishBuy = () => {
             const ventasCollection = collection(db, 'ventas');
             addDoc(ventasCollection,{
@@ -29,7 +34,9 @@ import { cartContext } from "../Context/CartContext";
                 items: products,
                 date: serverTimestamp(),
                 cantidad: qtyProducts,
+                total: total,
             })
+
             .then((result) => {
                 setidVenta(result.id)
             })
@@ -38,23 +45,26 @@ import { cartContext } from "../Context/CartContext";
 
         return (
             <>
-            <h3>Ingresa tus datos</h3>
-            <form onSubmit={finishBuy}>
-                <label>Nombre</label>
-                <input onChange={createidVenta} type="text" name="nombre" className="form-control" required></input>
-                <label>Apellido</label>
-                <input onChange={createidVenta} type="text" name="apellido" className="form-control" required></input>
-                <label>Telefono</label>
-                <input onChange={createidVenta} type="number" name="telefono" className="form-control" required></input>
-                <label>Email</label>
-                <input onChange={createidVenta} type="email" name="email" className="form-control"></input>
-                <label>Observaciones</label>
-                <input onChange={createidVenta} type="message" name="observaciones" className="form-control"></input>
-                <submit onClick={finishBuy} className="btn btn-success" type="submit">
-                    Finalizar Compra
-                </submit>
-            </form>
+                <h3 className="font-bold text-2xl text-center m-5">Ingresa tus datos</h3>
+                <hr className="divider"/>
+                <form onSubmit={finishBuy} className="flex flex-col justify-content m-10 gap-3 bg-red-50">
+                    <label>Nombre</label>
+                    <input onChange={createidVenta} type="text" name="nombre" className="bg-red-100" required></input>
+                    <label>Apellido</label>
+                    <input onChange={createidVenta} type="text" name="apellido" className="bg-red-100" required></input>
+                    <label>Telefono</label>
+                    <input onChange={createidVenta} type="number" name="telefono" className="bg-red-100" required></input>
+                    <label>Email</label>
+                    <input onChange={createidVenta} type="email" name="email" className="bg-red-100"></input>
+                    <label>Observaciones</label>
+                    <input onChange={createidVenta} type="message" name="observaciones" className="bg-red-100"></input>
+                    <submit onClick={finishBuy} className="m-5 p-2 text-center font-light bg-red-200 text-black drop-shadow-md rounded-md overflow-hiddenr" type="submit">
+                        Finalizar Compra
+                    </submit>
+                </form>
             </>
         );
         }
+
+
 export default Formulario
